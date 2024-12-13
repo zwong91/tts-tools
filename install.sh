3bde66cbae45f5dc22e2003c91d3062e645ccf2c
zwong91 <yzbingchuan@gmail.com>
2024-12-14 04:01:10 +0800

    更新 README 和安装脚本，将 PyTorch 版本修改为 2.2.1

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 conda install -c conda-forge gcc
 conda install -c conda-forge gxx
 conda install ffmpeg cmake
-conda install pytorch==2.5.1 torchvision==0.16.1 torchaudio==2.5.1 pytorch-cuda=12.2 -c pytorch -c nvidia
+conda install pytorch==2.2.1 torchvision==0.16.1 torchaudio==2.2.1 pytorch-cuda=12.2 -c pytorch -c nvidia
 pip install -r requirements.txt
