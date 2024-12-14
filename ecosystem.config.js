5f9dd27933874a1e429af289c0ebf9756205b1d3
jong-un <36259838+zwong91@users.noreply.github.com>
2024-12-14 22:31:39 +0800

    pm2 tasks

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [
+      {
+        name: "urv5-tasks",            // 任务名称
+        script: "python3",             // 使用 Python 执行脚本
+        args: "gradio_task.py",        // 执行的 Python 脚本
+        cron_restart: "*/5 * * * *",   // 每 5 分钟执行一次
+        autorestart: true,             // 如果脚本崩溃，自动重启
+        watch: false,                  // 禁止文件变化时自动重启
+        max_memory_restart: "1G",      // 超过 1GB 内存时重启脚本
+      }
+    ]
+  };
+  
